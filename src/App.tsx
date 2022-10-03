d1e83f02188cc906d4e015f37b413aaf9fee2d65
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-10-03 13:57:07 +0200

    create a footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <div className="App">
        <h1 className="text-3xl font-bold underline">
           Hello world!
       </h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <div>
+        <p>
+          Copyright © 2022 -
+          <a className="link link-hover" href="https://github.com/guillaume-gomez">
+            Made by Guillaume Gomez
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
