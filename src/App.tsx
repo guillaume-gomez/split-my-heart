f7cf0fb5e312ce157e2c98caecc09bcd01ccae7a
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-10-09 20:24:57 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import './App.css';
 
 import Form from "./Components/Form";
 import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="flex flex-col gap-5 items-center">
+        <Header />
         <Form />
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 }
