0280565f90e0880cda1f9413b3bfba9022be085b
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-10-21 23:15:54 +0200

    update header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from "react-router-dom";
 
 function Header() {
+
+
+
   return (
     <div className="navbar bg-base-100">
   <div className="navbar-start">
