2983fd157616642d06258dbb5ff573b9c06bb776
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-10-25 18:10:16 +0200

    update the view that render the heart result

---
+++ src/Views/FillYourHeartResult.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SplitMyHeartCanvas from "../Components/SplitMyHeartCanvas";
+
+function FillYourHeartResult() {
+  return (
+        <SplitMyHeartCanvas />
+  );
+}
+
+export default FillYourHeartResult;
