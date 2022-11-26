cf6a1047cc52d054576a81169e1545dbe0a183d3
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-11-26 15:26:36 +0100

    make the color picker works

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const COLORS = ['gray', 'red','yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
+const VARIANTS = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+const safelist = []
+COLORS.forEach((color) => {
+  VARIANTS.forEach((variant) => {
+    safelist.push(`bg-${color}-${variant}`)
+  })
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    safelist
+  },
   theme: {
     extend: {},
   },
