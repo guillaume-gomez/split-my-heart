0d9b826b3fdc987b530dfa9c0bdd476b7c517f9d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-01-01 20:10:26 +0100

    flex column in the layout'

--- src/Views/ReactRouterRoot.tsx
+++ src/Views/ReactRouterRoot.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
 
-
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
-
-
-
 
 function ReactRouterRoot() {
   return (
       <div className="flex flex-col gap-5 h-screen items-center">
         <Header />
-         <div className="flex-grow flex items-center w-10/12">
+         <div className="flex-grow flex flex-col gap-5 items-center w-10/12">
           <Outlet/>
          </div>
         <Footer />
