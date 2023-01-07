ef83e23a319b37b15f39bb31d4cd058945e98b14
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-01-07 14:53:38 +0100

    fix pop up interactions

--- src/Components/PopUp.tsx
+++ src/Components/PopUp.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface PopUpProps {
-  onClick: () => void;
+  //onClose: () => void;
 }
 
-function PopUp({onClick} : PopUpProps) {
+function PopUp({  } : PopUpProps) {
+  const [open, setOpen] = useState<boolean>(true);
+
   return (
-    <div className="modal modal-open">
+    <div className={`modal ${open ? "modal-open" : ""}`}>
       <div className="modal-box relative">
-        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2" onClick={() => setOpen(false)}>✕</label>
         <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
         <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
       </div>
