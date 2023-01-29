fb2dfe5d3ca0d4e4d98ca639779e261e311a45c5
Guillaume Gomez <guillaume.gomez846@gmail.com>
2023-01-29 23:46:37 +0100

    finish the lover card

--- src/Components/LoverCard.tsx
+++ src/Components/LoverCard.tsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { dataStats } from "./SplitMyHeartCanvas";
-import LegendPart from "./LegendPart";
+import { LoverInterface } from "./Form";
+import ResponsiveRange from "./ResponsiveRange";
+import ColorPicker from "./ColorPicker";
 
 interface LoverCardInterface {
-  loversData: dataStats[];
+  lover: LoverInterface;
+  deleteLover: () => void;
+  changeLover: (newLover: LoverInterface) => void;
 }
 
-function LoverCard({loversData} : LoverCardInterface) {
+function LoverCard({lover, changeLover, deleteLover } : LoverCardInterface) {
+  function onChangeLoverRate(value: number, lover: LoverInterface) {
+    changeLover({...lover, percentage: value});
+  }
+
+  function onChangeLoverName(value: string, lover: LoverInterface) {
+    changeLover({...lover, name: value});
+  }
+
+  function onChangeLoverColor(value: string, lover: LoverInterface) {
+    changeLover({...lover, color: value});
+  }
+
   return (
-    <div className="card bg-base-300 flex flex-col gap-5 items-center p-2">
-      {
-        loversData.map((loverData, index) =>
-          <LegendPart
-            key={`${loverData.name}-${index}`}
-            label={`${loverData.name}  ${loverData.percentage}%`}
-            color={loverData.color}
+    <div className="indicator">
+      <div className="indicator-item indicator-bottom">
+          <button className="btn btn-secondary" onClick={() => deleteLover()}>➖</button>
+      </div>
+      <div className="card w-96 bg-base-100 text-primary shadow-xl" style={{border: `8px solid ${lover.color}`}}>
+        <div className="card-body">
+          <ColorPicker onChange={(value) => onChangeLoverColor(value, lover)}/>
+          <div className="form-control max-w-xs">
+              <label className="label">
+                <span className="label-text">Your lover's name</span>
+              </label>
+              <input
+                type="text"
+                className="input input-primary sm:input-md input-sm input-bordered"
+                value={lover.name}
+                onChange={(e) => onChangeLoverName(e.target.value, lover)}
+              />
+          </div>
+          <ResponsiveRange
+            onChange={(value) => onChangeLoverRate(value, lover)}
+            value={lover.percentage}
           />
-        )
-      }
+        </div>
+      </div>
     </div>
   );
 }
